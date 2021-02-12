511e2d13427b963742ed73deb88711ad75c38737
Matt <madas@hawk.iit.edu>
2021-02-12 12:32:16 -0600

    Initial Commit

---
+++ Shared/sin_x_App.swift
@@ -0,0 +1,17 @@
+//
+//  sin_x_App.swift
+//  Shared
+//
+//  Created by Matthew Adas on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct sin_x_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
